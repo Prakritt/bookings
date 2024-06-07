b28f8114efdd362a335b4df5e2d71e6dc6d863d9
Prakrit Uprety <prakritupreety0@gmail.com>
2024-06-07 16:38:31 -0400

    Complete Basic Layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -16,15 +17,17 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+            <Route path="account" element={<Account />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
-          <Route path="account" element={<Account />} />
-          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
