dadb0c3af8b81ee762f04b30c6749800d72641ec
Prakrit Uprety <prakritupreety0@gmail.com>
2024-06-11 16:26:04 -0400

    Add Feature : Delete Cabin, Toast alert

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,12 @@ import Users from "./pages/Users";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
@@ -41,6 +42,18 @@ function App() {
           <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={8}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
